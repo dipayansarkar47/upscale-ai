dc9ff0891a24581919c8cffa172d3b3a79c2de0e
dipayansarkar47 <dipayansarkar265482@gmail.com>
2024-01-11 18:29:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [imageFile, setImageFile] = useState(null);
+  const [upscaledImage, setUpscaledImage] = useState(null);
+  const [Loading, setLoading] = useState(false);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+  };
+
+const upscaleImage = async () => {
+  const formData = new FormData();
+  formData.append('image', imageFile);
+  formData.append('sizeFactor', '2');
+  formData.append('imageStyle', 'default');
+  formData.append('noiseCancellationFactor', '0');
+
+  const options = {
+    method: 'POST',
+    url: 'https://ai-picture-upscaler.p.rapidapi.com/supersize-image',
+    headers: {
+      'X-RapidAPI-Key': '824dddee65msh76dbe040b14bfe4p12ab6ajsnc6532155d4f8',
+      'X-RapidAPI-Host': 'ai-picture-upscaler.p.rapidapi.com',
+      'Content-Type': 'multipart/form-data',
+    },
+    data: formData,
+    responseType: 'arraybuffer',
+  };
+
+  try {
+    setLoading(true);
+    const response = await axios.request(options);
+    const base64Image = btoa(
+      new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+    );
+    setUpscaledImage(base64Image);
+    setLoading(false);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// const handleDownload = () => {
+//   const url = window.URL.createObjectURL(new Blob([upscaledImage]));
+//   const link = document.createElement('a');
+//   link.href = url;
+//   link.setAttribute('download', 'upscaled_image.jpg');
+//   document.body.appendChild(link);
+//   link.click();
+// };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex  h-screen flex-col p-10 gap-5 justify-center items-center">
+      <h1 className='text-5xl animate-text p-2 font-serif bg-gradient-to-r from-indigo-700 via-purple-700 to-cyan-300 inline-block text-transparent bg-clip-text font-bold'>AI Image Upscaler</h1>
+      <input className='block p-2 text-sm text-white border-dashed border-2 border-gray-400 rounded-lg cursor-pointer outline-none ' type="file" accept="image/*" onChange={handleImageChange}  />
+      <button className='animate-text font-mono bg-gradient-to-l from-indigo-700 via-purple-700 to-cyan-300 text-white p-2 rounded' onClick={upscaleImage}>Upscale Image</button>
+      { Loading && 
+        <div role="status">
+        <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span class="sr-only">Loading...</span>
+    </div>
+      }
+      {upscaledImage && (
+        <div className='flex flex-col justify-center items-center m-5 p-5'>
+          <img src={`data:image/jpeg;base64,${upscaledImage}`} className=' rounded-lg shadow-lg aspect-auto h-56' alt="Upscaled" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
